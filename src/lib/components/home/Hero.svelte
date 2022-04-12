a348bb1af2a4af714af709a918e3ebb8901ba8b2
LUIS ANAYA <brothersailorwebdev@gmail.com>
2022-04-12 02:16:23 +0200

    changed font type in text

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -1,7 +1,7 @@
 <div class="tw-full-page-desktop flex items-center justify-center">
 	<h1 class="flex flex-col font-heading text-7xl font-bold lg:text-9xl">
 		<span class="lg:-ml-24">Juan</span>
-		<span class="font-sans font-thin italic text-highlight">Serrano</span>
+		<span class="font-medium italic text-highlight">Serrano</span>
 		<span class="lg:ml-24">Studio</span>
 	</h1>
 </div>
