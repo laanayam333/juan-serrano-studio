92cb8ceee3e6bbf8ea17d693c517f19fef77b08b
LUIS ANAYA <brothersailorwebdev@gmail.com>
2022-04-12 13:44:26 +0200

    added tagline

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -1,7 +1,10 @@
-<div class="tw-full-page-desktop flex items-center justify-center">
+<div class="tw-full-page-desktop flex flex-col items-center justify-center">
 	<h1 class="flex flex-col font-heading text-7xl font-bold lg:text-9xl">
 		<span class="lg:-ml-24">Juan</span>
 		<span class="font-medium italic text-highlight">Serrano</span>
 		<span class="lg:ml-24">Studio</span>
+		<span class="mt-4 font-sans text-lg font-light tracking-widest md:mt-6 md:self-end md:text-2xl"
+			>Muebles Artesanos</span
+		>
 	</h1>
 </div>
